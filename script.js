66b77390a701dfb9027f636f426219e8d80837e0
Sander van Meerveld <sander.van.meerveld@gmail.com>
2021-05-20 14:50:07 +0200

    return toegevoegd werkt nog steeds niet hij print 4 ipv de array

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const addTheWordCool = function(array){
     // add your code
-    array.push("cool");
+    return array.push("cool");
 }
 console.log("Add cool:", addTheWordCool(["nice", "awesome", "tof"]));
 // resultaat: ["nice", "awesome", "tof", "cool"]
