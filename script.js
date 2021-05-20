3fc548a27ba9953b9087d07b37845d1be23be647
Sander van Meerveld <sander.van.meerveld@gmail.com>
2021-05-20 14:58:42 +0200

    met dank aan Donatas Rasiukevičius werkt het nu wel

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const addTheWordCool = function(array){
     // add your code
-    return addTheWordCool.push("cool");
+    array.push("cool");
+    return array;
 }
 console.log("Add cool:", addTheWordCool(["nice", "awesome", "tof"]));
 // resultaat: ["nice", "awesome", "tof", "cool"]
