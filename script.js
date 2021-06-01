23280f8f1c12a043ca4342c956c2c4487a202484
Sander van Meerveld <sander.van.meerveld@gmail.com>
2021-06-01 13:52:55 +0200

    added opdracht C

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// OPDRACHT A
 const addTheWordCool = function(array){
     // add your code
     array.push("cool");
@@ -6,17 +7,22 @@ const addTheWordCool = function(array){
 console.log("Add cool:", addTheWordCool(["nice", "awesome", "tof"]));
 // resultaat: ["nice", "awesome", "tof", "cool"]
 
-const  amountOfElementsInArray = ['appels', 'peren', 'citroenen'];
+// OPDRACHT B
 
-// const totalElementsInArray = amountOfElementsInArray.reduce((currentTotal, item ) => {
-//     return
-// }   )
+const  amountOfElementsInArray = ['appels', 'peren', 'citroenen'];
 
 console.log(amountOfElementsInArray.length);
 
-
-
 // console.log(amountOfElementsInArray(['appels', 'peren', 'citroenen'])); 
+
+//OPDRACHT C 
+
+selectBelgiumFromBenelux = ["Belgie", "Nederland", "Luxemburg"];
+
+console.log(selectBelgiumFromBenelux[0]);
+
+// resultaat: "Belgie" 
+
 
 
 
