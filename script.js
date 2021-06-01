612cdd8223195b06fb161c58556e124eb1d6f5dc
Sander van Meerveld <sander.van.meerveld@gmail.com>
2021-06-01 14:19:01 +0200

    added opdracht D slice

--- script.js
+++ script.js
@@ -24,6 +24,35 @@ console.log(selectBelgiumFromBenelux[0]);
 // resultaat: "Belgie" 
 
 
+// OPDRACHT D
+
+const lastElementInArray = ["Haas", "Cavia", "Kip", "Schildpad"];
+
+console.log(lastElementInArray[3])
+
+  // resultaat: "Schildpad"
+
+  //OPDRACHT E 
+
+  const presidents = ["Trump", "Obama", "Bush", "Clinton"] 
+  
+  const impeachTrumpSlice = function(array) {
+                    return presidents.slice(1)
+  }
+
+
+
+//   const impeachTrumpSplice = function(array) {
+//      console.log(presidents.splice(0, 1 , ""));
+//      return  presidents.splice(0, 1, "");
+//         // console.log(presidents)
+//   }
+  
+  console.log(impeachTrumpSlice(presidents)); // ["Obama", "Bush", "Clinton"]
+// //   console.log(impeachTrumpSplice(presidents)); // ["Obama", "Bush", "Clinton"]
+// // impeachTrumpSplice();
+// console.log(impeachTrumpSplice(presidents));
+
 
 
 
