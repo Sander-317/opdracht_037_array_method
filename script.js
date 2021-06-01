cef2ab312965f1244a1a988337f068f094609efd
Sander van Meerveld <sander.van.meerveld@gmail.com>
2021-06-01 13:45:00 +0200

    added opdracht B

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ const addTheWordCool = function(array){
 }
 console.log("Add cool:", addTheWordCool(["nice", "awesome", "tof"]));
 // resultaat: ["nice", "awesome", "tof", "cool"]
+
+const  amountOfElementsInArray = ['appels', 'peren', 'citroenen'];
+
+// const totalElementsInArray = amountOfElementsInArray.reduce((currentTotal, item ) => {
+//     return
+// }   )
+
+console.log(amountOfElementsInArray.length);
+
+
+
+// console.log(amountOfElementsInArray(['appels', 'peren', 'citroenen'])); 
+
+
+
+
